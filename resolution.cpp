0842a035653212947a3a7b990f5581376ba20fd6
Anastasia Fedorova <79627149+anasfreud@users.noreply.github.com>
2022-12-13 09:27:53 +0300

    Update resolution.cpp

--- resolution.cpp
+++ resolution.cpp
@@ -1,222 +1,232 @@
 #include <iostream>
+#include <vector>
 #include <string>
+#include <set>
 #include <random>
 #include <cmath>
 #include <algorithm>
 
 using namespace std;
 
-#define BASE 3
+#define MAX_DIS 20
 
-int fromTernary(int* a, int n) {
-    int x = 0;
-    for (int i = 0; i < n; i++) {
-        x += a[i] * pow(3, n - i - 1);
-    }
-    return x;
+int cnt = 0;
+
+void strToSet(string s, set<vector<int>>& d) {
+	cnt++;
+	int size = 0;
+	for (int i = 0; i < s.length(); i++) {
+		cnt++;
+		if (s[i] == 'x') {
+			int p = 0;
+			i++;
+			cnt += 3;
+			while (isdigit(s[i])) {
+				p = p * 10 + (s[i] - '0');
+				i++;
+				cnt += 6;
+			}
+			i--;
+			size = max(size, p);
+			cnt += 4;
+		}
+	}
+
+	vector<int> v(size, 0);
+	bool t = true;
+	cnt++;
+
+	for (int i = 0; i < s.length(); i++) {
+		char c = s[i];
+		cnt ++;
+
+		switch (c)
+		{
+		case '&':
+			cnt += 2;
+			d.insert(v);
+			v.assign(size, 0);
+			break;
+		case '-':
+			t = false;
+			i++;
+			cnt += 3;
+		case 'x':
+			int p = 0;
+			i++;
+			cnt += 3;
+			while (isdigit(s[i])) {
+				p = p * 10 + (s[i] - '0');
+				i++;
+				cnt += 6;
+			}
+			i--;
+			v[p - 1] = t + 1;
+			t = true;
+			cnt += 6;
+			break;
+		}
+	}
+	cnt++;
+	d.insert(v);
 }
 
-struct Formula {
-    int maxDisCnt = 0;
-    int varCnt = 0;
+string setToStr(set<vector<int>> d) {
+	cnt++;
+	string result = "";
+	for (auto it = d.begin(); it != d.end(); it++) {
+		vector<int> a = *it;
+		result += "(";
+		cnt += 2;
 
-    bool* d;
-    int** table;
+		for (int i = 0; i < a.size(); i++) {
+			cnt++;
+			if (a[i] == 1) {
+				cnt += 5;
+				result += "-x" + to_string(i + 1) + "|";
+			}
+			else if (a[i] == 2) {
+				cnt += 6;
+				result += "x" + to_string(i + 1) + "|";
+			}
+		}
+		cnt += 2;
+		if (result[result.length() - 1] != '(') {
+			cnt += 2;
+			result.erase(result.length() - 1);
+		}
+		cnt += 2;
+		result += ")&";
 
-    void generate(string s) {
-        makeTable();
-        fromString(s);
-    }
+	}
+	cnt++;
+	if (result.length() > 0) {
+		cnt += 2;
+		result.erase(result.length() - 1);
+	}
+	return result;
+}
 
-    void generate(int n) {
-        varCnt = n;
-        maxDisCnt = pow(3, n);
+string KNF(int n) {
 
-        makeTable();
+	set<vector<int>> d;
+	cnt += 2;
+	int disCnt = rand() % MAX_DIS;
 
-        d = new bool[maxDisCnt];
-        d[0] = true;
-        for (int i = 1; i < maxDisCnt; i++) {
-            d[i] = rand() % 2;
-        }
-    }
+	vector<int> v;
+	
+	for (int i = 0; i < disCnt; i++) {
+		cnt += 2;
+		v.clear();
+		int nulls = 0;
+		for (int j = 0; j < n; j++) {
+			cnt += 3;
+			v.push_back(rand() % 3);
+			if (v[j] == 0) {
+				cnt++;
+				nulls++;
+			}
+		}
+		cnt++;
+		if (nulls != v.size()) {
+			cnt++;
+			d.insert(v);
+		}
+	}
+	
+	return setToStr(d);
+}
 
-    void makeTable() {
-        table = new int* [maxDisCnt];
-        for (int i = 0; i < maxDisCnt; i++) {
-            table[i] = new int[varCnt];
-            int x = i;
-            for (int j = varCnt - 1; j >= 0; j--) {
-                table[i][j] = x % BASE;
-                x /= BASE;
-            }
-        }
-    }
+// возвращает false, если формула точно невыполнима, иначе true
+bool RES(string s) {
 
-    string getString() {
+	set<vector<int>> d;
+	strToSet(s, d);
 
-        int disCnt = 0;
-        for (int i = 1; i < maxDisCnt; i++) {
-            if (d[i]) {
-                disCnt++;
-            }
-        }
-        if (!disCnt) {
-            return "()";
-        }
+	bool changes = true;
+	cnt += 2;
 
-        string result = "";
-        makeTable();
-        for (int i = 1; i < maxDisCnt; i++) {
-            if (d[i]) {
-                result += "(";
+	while (changes) {
+		cnt++;
+		changes = false;
 
-                for (int q = 0; q < varCnt; q++) {
-                    if (table[i][q] == 1) {
-                        result += "-x" + to_string(q + 1) + "|";
+		for (auto it1 = d.begin(); it1 != d.end(); it1++) {
+			auto it2 = it1;
+			it2++;
+			cnt += 2;
+			
+			for (it2; it2 != d.end(); it2++) {
 
-                    } else if (table[i][q] == 2) {
-                        result += "x" + to_string(q + 1) + "|";
-                    }
-                }
-                result.erase(result.length() - 1, 1);
-                result += ")&";
-            }
-        }
-        result.erase(result.length() - 1, 1);
-        return result;
-    }
+				vector<int> a = *it1;
+				vector<int> b = *it2;
 
-    void resolution() {
+				int comm = 0;
+				int cont = 0;
+				cnt += 4;
+				for (int i = 0; i < a.size(); i++) {
+					cnt += 7;
+					if ((a[i] == 1 && b[i] == 2) || a[i] == 2 && b[i] == 1) {
+						cnt++;
+						cont++;
+					} else if (a[i] == b[i]) {
+						cnt += 2;
+						comm++;
+					} else {
+						break;
+					}
+				}
+				cnt += 4;
+				if (cont == 1 && comm == a.size() - 1) {
+					changes = true;
 
-        makeTable();
-        int changes = 1;
+					it2 = d.erase(it2);
+					it1 = d.erase(it1);
 
-        while (changes != 0) {
+					vector<int> c;
+					int nulls = 0;
+					cnt += 6;
+					for (int i = 0; i < a.size(); i++) {
+						cnt += 4;
+						c.push_back(a[i] == b[i] ? a[i] : 0);
+						if (c[i] == 0) {
+							nulls++;
+							cnt++;
+						}
+					}
 
-            changes = 0;
+					if (nulls == c.size()) {
+						cnt += 2;
+						d.clear();
+						d.insert(c);
+						return false;
+					}
 
-            for (int i = 0; i < maxDisCnt; i++) {
-                if (!d[i]) {
-                    continue;
-                }
+					d.insert(c);
+					cnt += 2;
 
-                for (int j = i + 1; j < maxDisCnt; j++) {
-                    if (!d[j]) {
-                        continue;
-                    }
-
-                    int cont = 0;
-                    int comm = 0;
-
-                    for (int q = 0; q < varCnt; q++) {
-                        if ((table[i][q] == 1 && table[j][q] == 2) || (table[i][q] == 2 && table[j][q] == 1)) {
-                            cont++;
-                        } else if (table[i][q] == table[j][q]) {
-                            comm++;
-                        } else {
-                            break;
-                        }
-                    }
-
-                    if (cont == 1 && comm == varCnt - 1) {
-                        int* b = new int[varCnt];
-
-                        for (int q = 0; q < varCnt; q++) {
-                            if (table[i][q] == table[j][q]) {
-                                b[q] = table[i][q];
-                            }
-                            else {
-                                b[q] = 0;
-                            }
-                        }
-
-                        d[i] = false;
-                        d[j] = false;
-                        int p = fromTernary(b, varCnt);
-
-                        if (p == 0) {
-                            memset(d, 0, maxDisCnt);
-                            return;
-                        }
-
-                        d[fromTernary(b, varCnt)] = true;
-
-                        changes++;
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
-    void fromString(string s)
-    {
-        int m = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == 'x') {
-                int p = 0;
-                i++;
-                while (isdigit(s[i])) {
-                    p = p * 10 + (s[i] - '0');
-                    i++;
-                }
-                i--;
-                m = max(m, p);
-            }
-        }
-
-        varCnt = m;
-        maxDisCnt = pow(3, m);
-
-        d = new bool[maxDisCnt];
-        memset(d, 0, maxDisCnt);
-
-        int* b = new int[varCnt];
-        memset(b, 0, varCnt * 4);
-
-        bool t = false;
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s[i];
-
-            switch (c) {
-            case '&':
-                d[fromTernary(b, varCnt)] = true;
-                memset(b, 0, varCnt * 4);
-                break;
-            case '-':
-                t = true;
-                i++;
-            case 'x':
-                int p = 0;
-                i++;
-                while (isdigit(s[i])) {
-                    p = p * 10 + (s[i] - '0');
-                    i++;
-                }
-                i--;
-                b[p - 1] = t ? 1 : 2;
-                t = false;
-                break;
-            }
-        }
-
-        d[fromTernary(b, varCnt)] = true;
-    }
-};
-
-
+					break;
+				}
+			}
+			cnt++;
+			if (changes) {
+				break;
+			}
+		}
+	}
+	return true;
+}
 
 int main()
 {
-    srand(time(nullptr));
-    
-    Formula f;
-    f.generate(2);
-    cout << f.getString() << endl;
-    f.resolution();
-    cout << f.getString() << endl;
+	srand(time(nullptr));
 
-    return 0;
+	set<vector<int>> d;
+
+	for (int n = 10; n <= 200; n += 10) {
+		cnt = 0;
+		RES(KNF(n));
+		cout << cnt << endl;
+	}
+
+	return 0;
 }
